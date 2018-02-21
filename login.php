2223ac96f0229f1bd3b690d67e856c423d44da00
Angel Avalos <leonavalos4772@gmail.com>
2018-02-20 22:11:33 -0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/css.css">
+	<title>login</title>
+</head>
+<body>
+	<div class="login-page">
+  <div class="form">
+
+      <form action="validar.php" method="post">
+      <input type="text" name="mail" placeholder="mail" />
+      <input type="password" name="pass" placeholder="password" />
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
+  </div>
+</div>
+
+</form>
+</body>
+</html>
---
+++ validar.php
@@ -0,0 +1,29 @@
+<?php
+//include("connect_db.php");
+
+//$miconexion = new connect_db;
+
+
+session_start();
+	require("connect.php");
+
+	$username=$_POST['mail'];
+	$pass=$_POST['pass'];
+
+
+	
+	$sql2=mysqli_query($mysqli,"SELECT * FROM users WHERE email='$username'");
+	if($f2=mysqli_fetch_assoc($sql2)){
+		if($pass==$f2['password']){
+			$_SESSION['id']=$f2['id'];
+			$_SESSION['tipo']=$f2['tipo'];
+
+			echo '<script>alert("BIENVENIDO ADMINISTRADOR")</script> ';
+			echo "<script>location.href='admin.php'</script>";
+		
+		}
+	}
+
+
+
+?>
